1655629f6fcf7f67232ea2dc53d5866a5031812f
Brian Chirls <brian@chirls.com>
2019-04-16 17:11:09 -0400

    Fix gh-pages deploy script

---
+++ gh-pages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname "$0")
+cd "$SCRIPT_DIR"
+git checkout master
+rm -rf gh-pages/*
+npx webpack --config ./config/webpack-prod.js --mode production -o gh-pages/aframe-ambisonic-component.js
+tar cf -  -C ./examples/ $(git ls-files examples | sed -e 's/^examples\//\.\//') | (cd ./gh-pages; tar xf -)
+npx gh-pages -d ./gh-pages
