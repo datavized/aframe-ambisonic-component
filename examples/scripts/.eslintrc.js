b75686032e01bdc47de602a666420107f4c4621a
Brian Chirls <brian@chirls.com>
2019-04-17 17:04:03 -0400

    different lint rules for example scripts
    ...since they're not transpiled

---
+++ examples/scripts/.eslintrc.js
@@ -0,0 +1,7 @@
+/* eslint-env node, browser: false */
+const eslintConfig = require('../../.eslintrc.js');
+module.exports = Object.assign(eslintConfig, {
+	rules: Object.assign(eslintConfig.rules, {
+		'no-var': 0
+	})
+});
